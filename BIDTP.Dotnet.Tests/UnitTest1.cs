527c60616ec4b87d0e5b78e80e6c96bec3e7d534
lol <_email@example.com>
2024-03-21 11:24:54 +0300

    Add DI container

---
+++ BIDTP.Dotnet.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BIDTP.Dotnet.Tests;
+
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
